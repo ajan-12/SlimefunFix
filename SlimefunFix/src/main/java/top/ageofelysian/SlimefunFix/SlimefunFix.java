d361e4d570d26a81a1f70f4b14b015f89a35c7c6
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2018-04-30 01:34:20 +0300

    Updated

--- SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
+++ SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SlimefunFix extends JavaPlugin {
@@ -16,12 +17,16 @@ public final class SlimefunFix extends JavaPlugin {
 			if (sender instanceof Player) {
 		        Player player = (Player) sender;
 		        PlayerInventory inventory = player.getInventory();
-		        //List of items to fix
-		        ItemStack $itemname = new ItemStack(Material.$item, 64);
-		        //the thing that will happen to the inventories
-		        if (inventory.contains($itemname)) {
-		        	
-		        }
+          //Checking if player has a skull item named CSCoreLib
+          ItemStack skullcheck = new ItemStack(Material.SKULL_ITEM, 1);
+          //if (OfflinePlayer.getOwningPlayer().equals("CSCoreLib")) {
+              //if (inventory.contains(skullcheck)) {
+		        	      
+              //}
+		        //}
+          private static String getSkullName(Skull skullcheck) {
+                 skullcheck.getOwningPlayer().getName();
+          }
 		    }
 			return true;
 		}
