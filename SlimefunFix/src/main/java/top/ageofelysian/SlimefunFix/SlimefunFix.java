4bec3c541f389d7324798b28f4fe89a002cb1dca
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2018-04-30 22:08:37 +0300

    Several fixes and additions

--- SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
+++ SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
@@ -6,9 +6,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
+@SuppressWarnings("unused")
 public final class SlimefunFix extends JavaPlugin {
     
 	@Override
@@ -17,20 +19,13 @@ public final class SlimefunFix extends JavaPlugin {
 			if (sender instanceof Player) {
 		        Player player = (Player) sender;
 		        PlayerInventory inventory = player.getInventory();
-        //Checking if player has a skull item named CSCoreLib
-        ItemStack skullcheck = new ItemStack(Material.SKULL_ITEM, 1);
-        //if (OfflinePlayer.getOwningPlayer().equals("CSCoreLib")) {
-              //if (inventory.contains(skullcheck)) {
-		        	      
-              //}
-        //}
-	private static String getSkullName(Skull skullcheck) {
-                 skullcheck.getOwningPlayer().getName();
-        }
+		        ItemStack glitcheditem = new ItemStack(Material.SKULL_ITEM, 1);
+				if (inventory.contains(glitcheditem) && glitcheditem.getItemMeta().getDisplayName().equals("CSCoreLib")) {
+					
 		        }
+		    }
 			return true;
 		}
 		return false; 
 	}
-	
 }
