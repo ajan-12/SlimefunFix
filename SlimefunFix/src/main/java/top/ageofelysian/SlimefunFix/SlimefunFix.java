fa0cf3e5571aa1c043e191237bd1448bde55e75a
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2018-04-30 22:44:45 +0300

    Added an easteregg
    added a command(/lightningbolt) that executes a lightning bolt wherever player looks at in a 200 blocks range

--- SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
+++ SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
@@ -1,9 +1,14 @@
 package top.ageofelysian.SlimefunFix;
 
+import java.util.Set;
+
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,7 +17,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 @SuppressWarnings("unused")
 public final class SlimefunFix extends JavaPlugin {
-    
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("sffix")) { // If the player typed /sffix then do the following
@@ -21,11 +26,18 @@ public final class SlimefunFix extends JavaPlugin {
 		        PlayerInventory inventory = player.getInventory();
 		        ItemStack glitcheditem = new ItemStack(Material.SKULL_ITEM, 1);
 				if (inventory.contains(glitcheditem) && glitcheditem.getItemMeta().getDisplayName().equals("CSCoreLib")) {
-					
+					OfflinePlayer owner = glitcheditem.getItemMeta(getSkullMeta()).getOwningPlayer();
 		        }
 		    }
 			return true;
 		}
-		return false; 
+		return false;
+		
+		if (cmd.getName().equalsIgnoreCase("lightningbolt")) {
+			Player player = (Player) sender;
+			// Creates a bolt of lightning at a given location. In this case, that location is where the player is looking.
+			// Can only create lightning up to 200 blocks away.
+			player.getWorld().strikeLightning(player.getTargetBlock((Set<Material>) null, 200).getLocation());
+		}
 	}
 }
