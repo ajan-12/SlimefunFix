b6a55da0d656467001bbcf4910f4f796a0d5a367
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2018-05-01 12:08:18 +0300

    1st Release -Needs testing-
    Changelog:
    - Added config files
    - Added removing items and etc.

--- SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
+++ SlimefunFix/src/main/java/top/ageofelysian/SlimefunFix/SlimefunFix.java
@@ -1,30 +1,26 @@
 package top.ageofelysian.SlimefunFix;
 
-import java.awt.List;
 import java.io.File;
-import java.util.Set;
 import java.util.ArrayList;
+import java.util.Set;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 
 
-@SuppressWarnings("unused")
 public final class SlimefunFix extends JavaPlugin {
 	
 	public void onEnable() {
-		getLogger().info(ChatColor.RED + "SlimefunFix loaded.");
+		getLogger().info(ChatColor.DARK_RED + "SlimefunFix loaded.");
 		try {
 	        if (!getDataFolder().exists()) {
 	            getDataFolder().mkdirs();
@@ -43,6 +39,7 @@ public final class SlimefunFix extends JavaPlugin {
 
 	}
 	
+	@SuppressWarnings("unlikely-arg-type")
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player player = (Player) sender;
@@ -54,14 +51,23 @@ public final class SlimefunFix extends JavaPlugin {
 					// If the player has the permission node sffix.use
 					if (player.hasPermission("sffix.use")) {
 						PlayerInventory inventory = player.getInventory();
-						ItemStack glitcheditem = new ItemStack(Material.SKULL_ITEM, 1);
+						// variable glitched item is a skull item
+						ItemStack glitcheditem = new ItemStack(Material.SKULL_ITEM);
+						int itemamount = glitcheditem.getAmount();
 						SkullMeta skullMeta = (SkullMeta)glitcheditem.getItemMeta();
+						// If the player has glitcheditem and its display name equals to CSCoreLib
 						if (inventory.contains(glitcheditem) && glitcheditem.getItemMeta().getDisplayName().equals("CSCoreLib")) {
 					        File file = new File(getDataFolder(), "items.yml");
+					        // variable owner is the owner of the item glitcheditem
 							OfflinePlayer owner = skullMeta.getOwningPlayer();
-							ArrayList<String> itemlist = (ArrayList<String>)getConfig().getStringList("items.owner");
-							if (itemlist.contains(owner)) {
-								(ArrayList<String>)getConfig().getStringList("items.owner")
+							ArrayList<String> itemowners = (ArrayList<String>)getConfig().getStringList("items.owners");
+							ArrayList<String> itemnames = (ArrayList<String>)getConfig().getStringList("items.names");
+							// If the arraylist itemowners has owner
+							if (itemowners.contains(owner) && itemowners.indexOf(owner) == itemnames.indexOf(owner)) {
+								int fixeditem = itemnames.indexOf(owner);
+								Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "sf give" + player + itemnames.get(fixeditem) + itemamount);
+								player.sendMessage(ChatColor.AQUA + "Your item" + ChatColor.RED + itemnames.get(fixeditem) + ChatColor.AQUA + "is fixed.");
+								glitcheditem.setAmount(0);
 							}
 						}
 					} else {
@@ -77,7 +83,8 @@ public final class SlimefunFix extends JavaPlugin {
 				if (sender instanceof Player) {
 					// If player has the permission node sffix.help
 					if (player.hasPermission("sffix.help")) {
-					
+						String help1 = "/sffix to fix your items.";
+						player.sendMessage(ChatColor.AQUA + help1);
 					}
 				return true;
 				}
